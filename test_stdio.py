343546083c6beba92287dd2bcd5dddcadc502cf7
krmt <ky@mbp.local>
2018-09-20 23:30:20 +0900

    add test for stdin/out/err

---
+++ test_stdio.py
@@ -0,0 +1,24 @@
+
+
+import sys
+
+
+def print_to_stdout(string):
+    print(string)
+
+
+def print_to_strerr(string):
+    sys.stderr.write(string)
+
+# TODO:use isatty()
+
+
+
+def test_stdin():
+    pass
+
+def test_stdout():
+    pass
+
+def test_stderr():
+    pass
